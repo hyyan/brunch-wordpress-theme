71e6cfaa3e4e2983f3dd956274639c33942b6326
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2014-11-01 20:30:16 -0700

    Added functions.php

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the hyyan/brunch-wordpress-theme package.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (!defined('ABSPATH'))
+    exit('restricted access');
+
+$brunch_includes = array(
+    'inc/constants.php', // theme constants
+    'inc/dependencies.php', // theme dependencies
+    'inc/setup.php', // setup theme features
+);
+
+foreach ($brunch_includes as $file) {
+    if (!$filepath = locate_template($file)) {
+        trigger_error(sprintf(__('Error locating %s for inclusion'), $file), E_USER_ERROR);
+    }
+
+    require_once $filepath;
+}
+unset($file, $filepath);
