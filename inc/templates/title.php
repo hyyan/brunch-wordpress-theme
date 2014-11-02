7b5056b4814718d7af3635a3837838d45b638ff3
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2014-11-01 23:13:39 -0700

    Added "title.php" template

---
+++ inc/templates/title.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the hyyan/brunch-wordpress-theme package.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Simple template to find out the best title 
+ */
+
+if (is_day()) :
+    printf(__('<span>Daily Archive</span> %s', BRUNCH_TEXTDOMAIN), get_the_date());
+elseif (is_month()) :
+    printf(__('<span>Monthly Archive</span> %s', BRUNCH_TEXTDOMAIN), get_the_date('F Y'));
+elseif (is_year()) : 
+    printf(__('<span>Yearly Archive</span> %s', BRUNCH_TEXTDOMAIN), get_the_date('Y'));
+elseif (is_search()) : 
+    printf(__('Search Results for: %s', BRUNCH_TEXTDOMAIN), '<span>' . get_search_query() . '</span>');
+elseif (is_category()) :
+    echo single_cat_title();
+elseif (is_tag()) :
+    echo single_tag_title();
+elseif (is_home()) :
+    _e('Latest Posts', BRUNCH_TEXTDOMAIN);
+elseif (is_author()) :
+    the_author();
+endif;
