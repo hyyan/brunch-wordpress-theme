2665c65bbe9dea622af4eae51eb880af9739fba3
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2014-11-01 20:31:14 -0700

    added header.php

---
+++ header.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * This file is part of the hyyan/brunch-wordpress-theme package.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (!defined('ABSPATH'))
+    exit('restricted access');
+?>
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8) ]><!-->
+<html <?php language_attributes(); ?>>
+    <!--<![endif]-->
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?php wp_title('|', true, 'right'); ?></title>
+
+        <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+        <?php wp_deregister_script('jquery') ?>
+        <?php wp_head(); ?>
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body <?php body_class(); ?>>
