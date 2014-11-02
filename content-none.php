866131c26d3ebb7c20a9e2da2f54e6032f2f2dec
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2014-11-02 00:23:05 -0700

    Added "content-none.php" file

---
+++ content-none.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * This file is part of the hyyan/brunch-wordpress-theme package.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (!defined('ABSPATH'))
+    exit('restricted access');
+?>
+
+<header class="page-header">
+    <h1 class="page-title"><?php _e('Nothing Found', BRUNCH_TEXTDOMAIN); ?></h1>
+</header>
+
+<div class="page-content">
+    <?php if (is_home() && current_user_can('publish_posts')) : ?>
+        <p><?php printf(__('Ready to publish your first post? <a href="%1$s">Get started here</a>.', BRUNCH_TEXTDOMAIN), admin_url('post-new.php')); ?></p>
+    <?php endif; ?>
+</div>
