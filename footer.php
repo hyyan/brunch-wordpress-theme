b41a885441cde317be35ccb3fa86698f91bd26b9
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2014-11-01 20:32:56 -0700

    Added footer.php

---
+++ footer.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This file is part of the hyyan/brunch-wordpress-theme package.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+if (!defined('ABSPATH'))
+    exit('restricted access');
+?>
+
+<?php wp_footer(); ?>
+<?php if (is_singular()) wp_print_scripts('comment-reply'); ?>
+</body>
+</html>
