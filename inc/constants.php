10053bfd0d94fdfc96751603bd67562f7d940789
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2014-11-01 20:28:35 -0700

    added constants.php

---
+++ inc/constants.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the hyyan/brunch-wordpress-theme package.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (!defined('ABSPATH'))
+    exit('restricted access');
+
+define('BRUNCH_THEME_NAME', 'Brunch');
+define('BRUNCH_TEXTDOMAIN', 'brunch');
