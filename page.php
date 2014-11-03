3a687cad94f49fae89ceb6167c12678621e2bac9
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2014-11-03 01:01:37 -0800

    Added "page.php" template

---
+++ page.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * This file is part of the hyyan/brunch-wordpress-theme package.
+ * (c) Hyyan Abo Fakher <tiribthea4hyyan@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+if (!defined('ABSPATH'))
+    exit('restricted access');
+?>
+
+<?php get_header(); ?>
+<div id="main-content" class="main-content">
+    <div id="primary" class="content-area">
+        <div id="content" class="site-content" role="main">
+
+            <?php
+            // Start the Loop.
+            while (have_posts()) : the_post();
+
+                // Include the page content template.
+                get_template_part('content', 'page');
+
+                // If comments are open or we have at least one comment, load up the comment template.
+                if (comments_open() || get_comments_number()) {
+                    comments_template();
+                }
+            endwhile;
+            ?>
+
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
